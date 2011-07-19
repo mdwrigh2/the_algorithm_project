2ee8dc7dd24f1156ce720488c6818c4170d1ffdb
Alex Ray <ajray@us.ibm.com>
2011-07-19 15:47:42 -0500

    fix go insertion sort

--- implementations/go/sorta/sort.go
+++ implementations/go/sorta/sort.go
@@ -1,15 +1,20 @@
 package sorta
 
+const MaxInt = int(^uint(0) >> 1) 
+
 func InsertionSort(A []int) {
 	for j := 1; j < len(A); j++ {
-		for i := j - 1; i > 0 && A[i] > A[j]; j-- {
+		key := A[j]
+		var i int
+		for i = j - 1 ; i >= 0 && A[i] > key; i-- {
 			A[i+1] = A[i]
 		}
+		A[i+1] = key
 	}
 }
 
 func IsSorted(A []int) bool {
-	for i := 0; i > len(A)-1; i++ {
+	for i := 0; i < len(A)-1; i++ {
 		if A[i] > A[i+1] {
 			return false
 		}
