7cff704d0535d486e260988de368c265fda44483
Michael Wright <mdwrigh2@ncsu.edu>
2011-07-23 19:43:14 -0700

    Documented insertion sort a bit better

--- implementations/python/algorithms/sorting.py
+++ implementations/python/algorithms/sorting.py
@@ -1,11 +1,28 @@
+# Insertion sort is a very naive sorting algorithm. Essentially it creates a
+# sorted sublist by traversing the list and inserting each element into the
+# sublist that is all elements previous to itself, in a sorted fashion. This
+# means that no matter what element you're on, all elements previous to it are
+# sorted. The insertion of the current element in this implementation is just a
+# linear search (i.e. traverse through the sorted sublist until you find the
+# proper location to insert the item). You could use binary search to find the
+# insertion point (which is known as binary insertion sort), but because you
+# have to shift elements in the array in order to insert the current element in
+# its proper location, the runtime doesn't change. Similarly, using a Linked
+# List allows for fast insertion, but slow search. Using more advanced data
+# structures, such as heaps, can improve the running time of this algorithm, and
+# is the basis for sorting methods such as heap sort.
+# RUNTIME: O(n^2), where n = len(arr)
 def insertion_sort(arr):
+    # Copy the array so we don't modify the original array
     sorted_arr = arr[:]
     for i in range(1, len(arr)):
         key = sorted_arr[i]
         j = i - 1
         while j >= 0 and sorted_arr[j] > key:
+            # Shift the element to the position to its right
             sorted_arr[j+1] = sorted_arr[j]
-            j = j - 1
+            # Move to the next element
+            j -= 1
         sorted_arr[j+1] = key
     return sorted_arr
 
