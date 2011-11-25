4ae022ab0cdc3cc40db0860cf048257e3ecf5a65
Alex Ray <ajray@ncsu.edu>
2011-11-25 00:12:54 -0500

    linked list structures and tests in Go, need to go and do them in C as well

---
+++ implementations/go/linkedlist/linkedlist.go
@@ -0,0 +1,114 @@
+// linkedlist.go - linked list functions/algorithms
+
+package linkedlist
+
+// DATA
+// ----
+type Data int
+
+func (d *Data) eq(a *Data) bool {
+	return *d == *a
+}
+
+// SINGLY LINKED LIST
+// ------------------
+type Node struct {
+	next  *Node
+	prev  *Node
+	child *Node
+	data  *Data
+}
+
+func NewNode(d *Data) *Node {
+	item := new(Node)
+	item.data = d
+	return item
+}
+
+func NewNodeData(d Data) *Node {
+	data := new(Data)
+	item := new(Node)
+	*data = d
+	item.data = data
+	return item
+}
+
+func Prepend(head **Node, d *Data) bool {
+	if head == nil { // bogus head, no list
+		return false
+	}
+	item := NewNode(d)
+	item.next = *head // handles empty or full lists
+	*head = item
+	return true
+}
+
+func Append(head **Node, d *Data) bool {
+	if head == nil { // bogus head, no list
+		return false
+	}
+	item := NewNode(d)
+	current := *head
+	var previous *Node
+	for current != nil { // traverse to end
+		previous = current
+		current = current.next
+	}
+	if previous == nil { // empty list
+		*head = item
+		return true
+	}
+	previous.next = item
+	return true
+}
+
+func AppendTail(head **Node, tail **Node, d *Data) bool {
+	// NOTE: Assumes head and tail are consistent
+	if head == nil || tail == nil { // bogus list
+		return false
+	}
+	item := NewNode(d)
+	if *tail == nil { // empty list
+		*tail = item
+		*head = item
+		return true
+	}
+	(*tail).next = item
+	*tail = item
+	return true
+}
+
+func Find(head *Node, d *Data) *Node {
+	current := head
+	for current != nil {
+		if d.eq(current.data) {
+			return current
+		}
+		current = current.next
+	}
+	return nil
+}
+
+func Delete(head **Node, delete *Node) bool {
+	if head == nil { // bogus head, no list
+		return false
+	}
+	if delete == nil { // bogus delete, you can always remove nil
+		return true
+	}
+	if *head == delete {
+		*head = delete.next
+		return true
+	}
+	current := *head
+	for current != nil {
+		if current.next == delete {
+			current.next = delete.next
+			// free(delete) but we're garbage-collected so not needed
+			return true
+		}
+		current = current.next
+	}
+	return false // didn't find it
+}
+
---
+++ implementations/go/linkedlist/linkedlist_test.go
@@ -0,0 +1,142 @@
+package linkedlist
+
+import "testing"
+
+type dataPair struct {
+	a, b Data
+	eq   bool
+}
+
+var dataPairs = []dataPair{
+	dataPair{Data(1), Data(1), true},
+	dataPair{Data(1), Data(2), false},
+	dataPair{Data(0), Data(0), true},
+	dataPair{Data(8), Data(0), false},
+}
+
+// just for testing's sake, yes I know it's pointless
+func TestDataEq(t *testing.T) {
+	for _, dp := range dataPairs {
+		res := dp.a.eq(&dp.b)
+		if dp.eq != res {
+			t.Errorf("Data.eq() %v == %v got %t, expected %t", dp.a, dp.b, res, dp.eq)
+		}
+	}
+}
+
+func TestPrepend(t *testing.T) {
+	// test failure case (no list at all, bogus head ptr)
+	if Prepend(nil, nil) {
+		t.Errorf("Prepend(nil,nil) got true, expected false")
+	}
+	// test empty list case (*head == nil)
+	var head *Node
+	Prepend(&head, nil)
+	if head == nil {
+		t.Errorf("Prepend() failed on empty list")
+	}
+	// test on actual list
+	head = new(Node)
+	old := head
+	Prepend(&head, nil)
+	if head == nil || head.next != old || head.next.next != nil {
+		t.Errorf("Prepend() failed on list")
+	}
+}
+
+func TestAppend(t *testing.T) {
+	// test failure case (no list at all, bogus head ptr)
+	if Append(nil, nil) {
+		t.Errorf("Append(nil,nil) got true, expected false")
+	}
+	// test empty list case (*head == nil)
+	var head *Node
+	Append(&head, nil)
+	if head == nil {
+		t.Errorf("Append() failed on empty list")
+	}
+	// test on actual list
+	head = new(Node)
+	old := head
+	Append(&head, nil)
+	if head != old || head.next == nil || head.next.next != nil {
+		t.Errorf("Append() failed on list")
+	}
+}
+
+func TestAppendTail(t *testing.T) {
+	// test failure case (no list at all, bogus head ptr)
+	if AppendTail(nil, nil, nil) {
+		t.Errorf("AppendTail(nil,nil) got true, expected false")
+	}
+	// test empty list case (*head == nil)
+	var head, tail *Node
+	AppendTail(&head, &tail, nil)
+	if head == nil || head != tail {
+		t.Errorf("AppendTail() failed on empty list")
+	}
+	// test on actual list
+	head = new(Node)
+	tail = head
+	old := head
+	AppendTail(&head, &tail, nil)
+	if head != old || head == tail || head.next != tail || tail.next != nil {
+		t.Errorf("AppendTail() failed on list")
+	}
+}
+
+func TestFind(t *testing.T) {
+	// test empty list case
+	if found := Find(nil,nil) ; found != nil {
+		t.Errorf("Find(nil,nil) got a pointer, expected nil")
+	}
+	// test one-item list
+	head := NewNodeData(1)
+	find := new(Data)
+	*find = 1
+	if found := Find(head,find) ; found != head {
+		t.Errorf("Find() failed on one-item list")
+	}
+	// test two-item list
+	head = NewNodeData(1)
+	tail := NewNodeData(2)
+	*find = 2
+	head.next = tail
+	if found := Find(head,find) ; found != tail {
+		t.Errorf("Find() failed on two-item list")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	// test bogus list case
+	if Delete(nil,nil) {
+		t.Errorf("Delete(nil,nil) got true, expected false")
+	}
+	// test bogus delete case
+	var head *Node
+	if !Delete(&head,nil) {
+		t.Errorf("Delete(&head,nil) got false, expected true")
+	}
+	// test delete head (one-item list) case
+	head = new(Node)
+	if !Delete(&head,head) || head != nil {
+		t.Errorf("Delete(&head,head) failed on one-item list")
+	}
+	// test delete not in list
+	head = new(Node)
+	tail := new(Node)
+	old := head
+	if Delete(&head,tail) || head != old {
+		t.Errorf("Delete(&head,tail) failed on delete not in list")
+	}
+	// test delete tail (two-item list) case
+	head = new(Node)
+	tail = new(Node)
+	old = head
+	head.next = tail
+	if !Delete(&head,tail) || head != old || head.next != nil {
+		t.Errorf("Delete(&head,tail) failed on two-item list")
+	}
+}
+
+
